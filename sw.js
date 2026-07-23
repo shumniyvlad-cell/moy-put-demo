cdcb4e15e33a817fec3dff8c5d15abe66881a3ea
Vlad Shuma <your-email@example.com>
2026-07-23 10:56:46 +0300

    Make journey facet motion visible

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mp-v14';
+const CACHE='mp-v20';
 const APP_SHELL=[
   './','./index.html','./manifest.json',
   './img/brand-mark.svg','./img/icon-192.png','./img/icon-512.png','./img/splash-hero.jpg','./img/dodecahedron-journey-v1.png',
