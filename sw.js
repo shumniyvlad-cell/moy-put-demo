8953c735aa6ffd8a20781aa6e8d7dc917cab9938
Vlad Shuma <your-email@example.com>
2026-07-28 20:40:00 +0300

    Create WAY visual variant A

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='mp-v33';
+const CACHE='mp-way-a-v2';
 const APP_SHELL=[
-  './','./index.html','./manifest.json',
-  './img/brand-mark.svg','./img/icon-192.png','./img/icon-512.png','./img/splash-hero.jpg','./img/dodecahedron-journey-v1.png',
+  './','./index.html','./way-a.css','./manifest.json',
+  './img/brand-mark.svg','./img/icon-192.png','./img/icon-512.png','./img/splash-hero.jpg','./img/dodecahedron-journey-v1.png','./img/way-a-splash-runner.png',
   './img/hero-1.jpg','./img/hero-2.jpg','./img/hero-3.jpg','./img/hero-4.jpg','./img/hero-5.jpg','./img/hero-6.jpg',
   './img/mentor-1.jpg','./img/mentor-2.jpg','./img/mentor-3.jpg','./img/mentor-4.jpg'
 ];
