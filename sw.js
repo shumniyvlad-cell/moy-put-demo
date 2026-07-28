82f8cc1c6e192b3292115c7c591eda7e219e18ce
Vlad Shuma <your-email@example.com>
2026-07-28 11:28:44 +0300

    Redesign home and mentor chat

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mp-v32';
+const CACHE='mp-v33';
 const APP_SHELL=[
   './','./index.html','./manifest.json',
   './img/brand-mark.svg','./img/icon-192.png','./img/icon-512.png','./img/splash-hero.jpg','./img/dodecahedron-journey-v1.png',
