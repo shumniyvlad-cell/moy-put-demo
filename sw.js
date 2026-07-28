ab1d1f8235fbeabc86ec3e24133ada22077e8f0a
Vlad Shuma <your-email@example.com>
2026-07-28 21:18:12 +0300

    Fix WAY A authentication controls

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mp-way-a-v2';
+const CACHE='mp-way-a-v3';
 const APP_SHELL=[
   './','./index.html','./way-a.css','./manifest.json',
   './img/brand-mark.svg','./img/icon-192.png','./img/icon-512.png','./img/splash-hero.jpg','./img/dodecahedron-journey-v1.png','./img/way-a-splash-runner.png',
